46b2210af2a01867b6c9b8df6dddfeeecb50b1b8
barry-ruprai <b.code@tutamail.com>
2025-05-28 23:05:53 -0400

    updated vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/ottawa-business-directory/',
+    base: mode === 'development' ? '/' : '/ottawa-business-directory/',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
